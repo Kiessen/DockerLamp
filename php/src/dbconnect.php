165e66a1731321bb9c15eb6c1f0cfa7864b333be
Ren <33624476+Kiessen@users.noreply.github.com>
2023-06-06 18:56:22 +0700

    first commit

---
+++ php/src/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+//mysql database connection
+$host = "host.docker.internal";
+$user = "root";
+$pass = "secret";
+$db = "db_crud";
+$port = "9906";
+//$con = mysqli_connect($host, $user, $pass, $db, $port) or die("Error " . mysqli_error($con));
+$con = new mysqli($host, $user, $pass, $db, $port);
+?>
