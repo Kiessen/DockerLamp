7a3c9da6da4f9fe53388bd2f7522eb3cb87f0e72
Ren <33624476+Kiessen@users.noreply.github.com>
2023-06-08 13:39:31 +0700

    3rd

--- php/src/dbconnect.php
+++ php/src/dbconnect.php
@@ -4,7 +4,6 @@ $host = "host.docker.internal";
 $user = "root";
 $pass = "secret";
 $db = "db_crud";
-$port = "9906";
-//$con = mysqli_connect($host, $user, $pass, $db, $port) or die("Error " . mysqli_error($con));
-$con = new mysqli($host, $user, $pass, $db, $port);
+$port = "3306";
+$con = mysqli_connect($host, $user, $pass, $db, $port) or die("Error " . mysqli_error($con));
 ?>
